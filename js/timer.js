7fd50512bc34d0512ee5296d0f8e00450bbdf1bb
Igor da Silva Medeiros <igoratendimento@outlook.com.br>
2022-07-25 21:34:45 -0300

    update seconds

--- js/timer.js
+++ js/timer.js
@@ -35,7 +35,7 @@ export default function Timer({
       }
 
       if (seconds <= 0) {
-        seconds = 10;
+        seconds = 60;
         --minutes;
       }
       updateDisplay(minutes, String(seconds - 1));
